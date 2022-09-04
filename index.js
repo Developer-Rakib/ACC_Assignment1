1978dcb02148882ee33ad77458132f99bdff21fc
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-09-04 09:28:45 +0600

    solve deployment issue

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.set("view engine", "ejs")
 
 
-app.use('/api/v1/user', router)
+app.use('/user', router)
 
 
 app.get("/", (req, res) => res.send("Welcome to ACC Assignment1"))
